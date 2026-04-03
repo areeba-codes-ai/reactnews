68bd94134831f0bff58531bd5639eb5bb324aa8d
areeba-codes-ai <areeba.codes@gmail.com>
2026-04-03 15:34:13 +0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    /* 1. Header tag yahan shuru ho raha hai aur saare styles iske andar hain */
+    <header style={{
+        backgroundColor: '#AE2448',
+        textAlign: 'center',
+        // padding: '10px 0 40px 0',
+        borderBottom: '2px solid white' // 2. Ye rahi aapki white line aur comma
+    }}>
+        <h1 style={{
+            color: '#D5E7B5',
+            fontWeight: "300",
+            margin: '0',
+            fontFamily: 'Impact',
+            letterSpacing: "0.5px",
+            
+        }}>
+            Awesome News
+        </h1>
+
+        <input 
+            type="text" 
+            placeholder="" 
+            style={{
+                backgroundColor: '#F3E8EE',
+                border: '2px solid #000',
+                height: '35px',
+                width: '280px',
+                margin: '10px auto',
+                display: 'block',
+                borderRadius: '4px',
+                textAlign: 'center',
+                fontSize: '1rem',
+                outline: 'none',
+                
+            }} 
+        />
+    </header>
+  );
+};
+
+export default Header;
